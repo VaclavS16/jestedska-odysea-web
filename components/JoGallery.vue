4ab94100618b206d54cf79cf41fbd1f6ce2c616c
vaclavstencl <vaclav.stencl@easysoftware.com>
2025-03-12 22:15:57 +0100

    galerie

---
+++ components/JoGallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="jo-gallery">
+    <JoGalleryItem v-for="item in galleryItems" :key="item.title" :item="item" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import galleryItems from "~/content/gallery.json"
+</script>
+
+<style scoped>
+.jo-gallery {
+  @apply rounded-md p-2 w-full bg-black  flex flex-col;
+}
+</style>
---
+++ pages/gallery/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <JoGallery />
+</template>
+
+<script setup>
+import JoGallery from "~/components/JoGallery.vue";
+
+definePageMeta({
+  layout: "default",
+});
+
+useHead({
+  title: "Galerie | Ještědská odysea 2025 | Festival, kde se sci-fi mění v realitu",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content: "Ještědská odysea je multižánrový hudební a kulturní festival, jenž se koná v Liberci.",
+    },
+  ],
+  htmlAttrs: {
+    lang: "cs",
+  },
+});
+</script>
